205e544de8e31fb420daa11480c912801e13fe6c
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-06-05 17:40:51 +0300

    Make 1st screen UI, two modal transitions

---
+++ WBMessenger/ModalView.swift
@@ -0,0 +1,18 @@
+//
+//  ModalView.swift
+//  WBMessenger
+//
+//  Created by Tatiana Lazarenko on 6/5/24.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModalView()
+}
---
+++ WBMessenger/WalkthroughView.swift
@@ -0,0 +1,18 @@
+//
+//  WalkthroughView.swift
+//  WBMessenger
+//
+//  Created by Tatiana Lazarenko on 6/5/24.
+//
+
+import SwiftUI
+
+struct WalkthroughView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WalkthroughView()
+}
