6e2291564f19969a77f9e4f8dc3f71905c24ed62
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-06-05 14:04:51 +0300

    Initial Commit

---
+++ WBMessenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WBMessenger
+//
+//  Created by Tatiana Lazarenko on 6/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WBMessenger/WBMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBMessengerApp.swift
+//  WBMessenger
+//
+//  Created by Tatiana Lazarenko on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WBMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
